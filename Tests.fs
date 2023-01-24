44131bd3fc3d73e1fd0a19a88abce4c640cdbf04
Giacomo Stevanato <giaco.stevanato@gmail.com>
2023-01-24 13:03:33 +0100

    Add simple literal type inference test

--- Tests.fs
+++ Tests.fs
@@ -1,11 +1,36 @@
 module Tests
 
 open Xunit
+open FSharp.Common
+open TinyML
+open TinyML.Ast
+open TinyML.Typing
+
+let parse_expr_from_string str = Parsing.parse_from_string SyntaxError str "TEST" (1, 1) Parser.program Lexer.tokenize Parser.tokenTagToTokenId
+
+let assert_inferred_type_eq str expected_ty =
+    let tenv = List.map (fun (n, t) -> (n, Forall (Set.empty, t))) gamma0
+    let expr = parse_expr_from_string str
+    let (expr_ty, _) = typeinfer_expr tenv expr
+    let normalize_ty t =
+        let rec normalize_ty_inner s t =
+            match t with
+            | TyName _ -> s, t
+            | TyArrow (t1, t2) ->
+                let s1, t1 = normalize_ty_inner s t1
+                let s2, t2 = normalize_ty_inner s1 t2
+                s2, TyArrow (t1, t2)
+            | TyVar tv ->
+                match List.tryFind (fun (tvk, _) -> tvk = tv) s with
+                | Some (_, tvs) -> s, TyVar tvs
+                | None -> (tv, List.length s) :: s, TyVar (List.length s)
+            | TyTuple ts ->
+                let s, ts = List.fold (fun (s, ts) t -> let s, t = normalize_ty_inner s t in (s, ts @ [t])) (s, []) ts
+                s, TyTuple ts
+        let _, t = normalize_ty_inner [] t
+        t
+    Assert.Equal ((normalize_ty expr_ty), (normalize_ty expected_ty))
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
-
-[<Fact>]
-let ``My test2`` () =
-    Assert.True(false)
+let ``Test literal TyInt`` () =
+    assert_inferred_type_eq "1" TyInt
